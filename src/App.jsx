e782e3b41cf1cab4518dee5d9b4e00d322c1b11f
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-04-08 23:46:43 +0600

    few updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ const App = () => {
       {/* <h1>Starting point</h1> */}
       {/* header */}
       <Header></Header>
+      <div className='min-h-[calc(100vh-136px)]'>
       <Outlet></Outlet>
+      </div>
       <Footer></Footer>
       {/* footer */}
      </div>
